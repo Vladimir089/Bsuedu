0e128fdd960e485d43d2ca208165e9e50f6eb751
Владимир <Pickerpikcer@gmail.com>
2024-01-30 22:59:56 +0300

    4 этаж полностью

--- IceRock/Model/12Corp.swift
+++ IceRock/Model/12Corp.swift
@@ -352,6 +352,159 @@ func twelverCorpus(path: UIBezierPath, greenPath: UIBezierPath, imageView: UIIma
             
         }
         
+        
+        
+        if corpGo == "12" && numbImage == 2 && etazGo == 4 {
+            
+            but1.isEnabled = true
+            but1.backgroundColor = UIColor(red: 0.9608, green: 0.8706, blue: 0.702, alpha: 1.0)
+            but2.isEnabled = false
+            but2.backgroundColor = .systemGray5
+            
+            imageView.image = nil
+            imageView.image = UIImage(named: "12corp4")
+            
+            DispatchQueue.global().async {
+                
+                if let coord = recognizeDigits(imageView: imageView, path: path) {
+                    DispatchQueue.main.async {
+                        
+                        greenPath.move(to: CGPoint(x: 0.14 * imageWidth, y: 0.44 * imageHeight))
+                        
+                        greenPath.addQuadCurve(to: CGPoint(x: 0.2 * imageWidth, y: 0.6 * imageHeight),
+                                          controlPoint: CGPoint(x: 0.15 * imageWidth, y: 0.55 * imageHeight))
+                        
+                        
+                        path.addArc(withCenter: CGPoint(x: 0.14 * imageWidth, y: 0.9 * imageHeight), radius: 5, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: true)
+                        path.move(to: CGPoint(x: 0.14 * imageWidth, y: 0.87 * imageHeight))
+                        path.addLine(to: CGPoint(x: 0.14 * imageWidth, y: 0.8 * imageHeight))
+                        path.move(to: CGPoint(x: 0.14 * imageWidth, y: 0.8 * imageHeight))
+                        path.addQuadCurve(to: CGPoint(x: 0.2 * imageWidth, y: 0.6 * imageHeight),
+                                          controlPoint: CGPoint(x: 0.14 * imageWidth, y: 0.62 * imageHeight))
+                        
+                        
+                        
+                        path.move(to: CGPoint(x: 0.2 * imageWidth, y: 0.6 * imageHeight))
+                        
+                        if cabGo == "4-4" || cabGo == "4-10" || cabGo == "4-12" || cabGo == "4-14" || cabGo == "4-6" || cabGo == "4-20" || cabGo == "4-18" || cabGo == "4-23" || cabGo == "4-8" {
+                            
+                            if cabGo == "4-4"    {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 5),
+                                                  controlPoint: CGPoint(x: coord.x + 5, y: 0.62 * imageHeight))
+                            }
+                            if cabGo == "4-10" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x + 20, y: coord.y - 5),
+                                                  controlPoint: CGPoint(x: coord.x + 5, y: 0.62 * imageHeight))
+                            }
+                            
+                            if cabGo == "4-12" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 10),
+                                                  controlPoint: CGPoint(x: coord.x + 5, y: 0.62 * imageHeight))
+                            }
+                            
+                            if cabGo == "4-14" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 27),
+                                                  controlPoint: CGPoint(x: coord.x + 5, y: 0.62 * imageHeight))
+                            }
+                            if cabGo == "4-6" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 17),
+                                                  controlPoint: CGPoint(x: coord.x + 5, y: 0.62 * imageHeight))
+                            }
+                            if cabGo == "4-20" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 5),
+                                                  controlPoint: CGPoint(x: coord.x + 5 , y: 0.5 * imageHeight))
+                            }
+                            if cabGo == "4-18" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x + 10, y: coord.y - 5),
+                                                  controlPoint: CGPoint(x: coord.x + 35 , y: 0.5 * imageHeight))
+                            }
+                            if cabGo == "4-23" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y + 28),
+                                                  controlPoint: CGPoint(x: coord.x - 5, y: 0.62 * imageHeight))
+                            
+                            }
+                            if cabGo == "4-8" {
+                                path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y - 10),
+                                                  controlPoint: CGPoint(x: coord.x - 5, y: 0.62 * imageHeight))
+                            
+                            }
+                        } else {
+                            path.addQuadCurve(to: CGPoint(x: coord.x, y: coord.y + 10),
+                                              controlPoint: CGPoint(x: coord.x - 5, y: 0.62 * imageHeight))
+                        }
+                        
+                        shapeLayerGreen.fillColor = .none
+                        shapeLayerGreen.path = greenPath.cgPath
+                        shapeLayerGreen.strokeColor = UIColor.systemGray5.cgColor
+                        shapeLayerGreen.lineWidth = 2.0
+                        imageView.layer.addSublayer(shapeLayerGreen)
+                        shapeLayer.path = path.cgPath
+                        shapeLayer.fillColor = .none
+                        shapeLayer.strokeColor = UIColor.systemGreen.cgColor
+                        shapeLayer.lineWidth = 2.0
+                        imageView.layer.addSublayer(shapeLayer)
+                        shapeLayerGreen.add(animation, forKey: "drawLineAnimation")
+                        shapeLayer.add(animation, forKey: "drawLineAnimation")
+                        
+                    }
+                }
+            }
+            
+        }
+        
+        
+        
+        
+        if corpGo == "12" && numbImage == 2 && etazGo == 5 {
+            
+            but1.isEnabled = true
+            but1.backgroundColor = UIColor(red: 0.9608, green: 0.8706, blue: 0.702, alpha: 1.0)
+            but2.isEnabled = false
+            but2.backgroundColor = .systemGray5
+            
+            imageView.image = nil
+            imageView.image = UIImage(named: "12corp5")
+            
+            DispatchQueue.global().async {
+                
+                if let coord = recognizeDigits(imageView: imageView, path: path) {
+                    DispatchQueue.main.async {
+                        
+//                        greenPath.move(to: CGPoint(x: 0.14 * imageWidth, y: 0.44 * imageHeight))
+//                        
+//                        greenPath.addQuadCurve(to: CGPoint(x: 0.2 * imageWidth, y: 0.6 * imageHeight),
+//                                          controlPoint: CGPoint(x: 0.15 * imageWidth, y: 0.55 * imageHeight))
+                        
+                        
+                        path.addArc(withCenter: CGPoint(x: 0.13 * imageWidth, y: 0.9 * imageHeight), radius: 5, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: true)
+                        path.move(to: CGPoint(x: 0.13 * imageWidth, y: 0.87 * imageHeight))
+                        path.addLine(to: CGPoint(x: 0.13 * imageWidth, y: 0.8 * imageHeight))
+                        path.move(to: CGPoint(x: 0.13 * imageWidth, y: 0.8 * imageHeight))
+                        path.addQuadCurve(to: CGPoint(x: 0.2 * imageWidth, y: 0.6 * imageHeight),
+                                          controlPoint: CGPoint(x: 0.13 * imageWidth, y: 0.62 * imageHeight))
+                        
+                        
+                        
+                        
+                        shapeLayerGreen.fillColor = .none
+                        shapeLayerGreen.path = greenPath.cgPath
+                        shapeLayerGreen.strokeColor = UIColor.systemGray5.cgColor
+                        shapeLayerGreen.lineWidth = 2.0
+                        imageView.layer.addSublayer(shapeLayerGreen)
+                        shapeLayer.path = path.cgPath
+                        shapeLayer.fillColor = .none
+                        shapeLayer.strokeColor = UIColor.systemGreen.cgColor
+                        shapeLayer.lineWidth = 2.0
+                        imageView.layer.addSublayer(shapeLayer)
+                        shapeLayerGreen.add(animation, forKey: "drawLineAnimation")
+                        shapeLayer.add(animation, forKey: "drawLineAnimation")
+                        
+                    }
+                }
+            }
+            
+        }
+        
     } else {
         print("тут пишем от кабинета до кабинета")
     }
